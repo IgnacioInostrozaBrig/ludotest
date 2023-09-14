396307aaea80e5cbedb7d04442a73dfa089630c2
Ignacio Inostroza <iinostroza1@miuandes.cl>
2023-09-14 20:16:10 -0300

    interface changes

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 from dice import Dice
 from board import Board
 from player import Player
+from interface import Interface
 import random
+
 
 class Game:
     def __init__(self):
@@ -14,22 +16,34 @@ class Game:
         self.players.clear()
         num_players = int(input("Ingrese la cantidad de jugadores (2-4): "))
         if num_players < 2 or num_players > 4:
-            print("Número de jugadores no válido. Debe ser entre 2 y 4.")
+            print("\033[91mNúmero de jugadores no válido. Debe ser entre 2 y 4.\033[00m")
             self.initialize_players()
             return
+        
+        playerCount = 0
         for i in range(1, num_players + 1):
             name = input(f"Ingrese el nombre del jugador {i}: ")
-            color = input(f"Ingrese el color del jugador {i}: ")
+            if (playerCount == 0):
+                color = "\033[34mazul\033[00m"
+            elif (playerCount == 1):
+                color = "\033[31mrojo\033[00m"
+            elif (playerCount == 2):
+                color = "\033[32mverde\033[00m"
+            else:
+                color = "\033[33mamarillo\033[00m"
             self.players.append(Player(name, color))
+            print(name + " ha sido inscrito con el color "+ color)
+            playerCount += 1
 
 
     def start(self):
+        Interface.start()
         self.initialize_players()
 
         # Determinar quién comienza lanzando el dado
         highest_roll = 0
         for player in self.players:
-            input(f"Presiona Enter para que {player.name} tire el dado...")
+            input(f"Presiona Enter para que {player.name} de color {player.color} tire el dado...")
             dice_value = self.dice.roll()
             print(f"{player.name} tiró un {dice_value}.")
             if dice_value > highest_roll:
@@ -39,7 +53,8 @@ class Game:
         print(f"{self.current_player.name} tiene el valor más alto y comienza el juego.")
 
         while True:
-            input(f"Presiona Enter para que {self.current_player.name} juegue...")
+            print(self.current_player)
+            input(f"Presiona Enter para que {self.current_player.name} de color {self.current_player.color} juegue...")
             dice_value = self.dice.roll()
             print(f"{self.current_player.name} tiró un {dice_value}.")
 
