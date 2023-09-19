af862e9936cb0f7dfeb7d9efadeb7fbe357d1055
DiegoChristie <diechristie@hotmail.com>
2023-09-19 20:19:18 -0300

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -3,6 +3,9 @@ class Ficha:
         self.player = player
         self.position = -1  # -1 indica que la ficha está en la Partida
         self.coronada = False
+        self.color = player.color
+        self.final_track = False
+        self.valor = 1
 
     def move(self, steps):
         # Implementa la lógica para mover la ficha según los pasos dados
